247427f02c266668d4b6376d60931d127366dce4
Sean LAIDLAW <seanlaidlaw95@gmail.com>
2024-11-06 01:07:18 +0000

    enable autosession in neovim

--- lua/plugins/1-base-behaviors.lua
+++ lua/plugins/1-base-behaviors.lua
@@ -171,28 +171,12 @@ return {
     opts = function()
       local config = require("session_manager.config")
       return {
-        autoload_mode = config.AutoloadMode.Disabled,
-        autosave_last_session = false,
+        autoload_mode = config.AutoloadMode.CurrentDir,
+        autosave_last_session = true,
         autosave_only_in_session = false,
       }
     end,
-    config = function(_, opts)
-      local session_manager = require("session_manager")
-      session_manager.setup(opts)
-
-      -- Auto save session
-      -- BUG: This feature will auto-close anything nofile before saving.
-      --      This include neotree, aerial, mergetool, among others.
-      --      Consider commenting the next block if this is important for you.
-      --
-      --      This won't be necessary once neovim fixes:
-      --      https://github.com/neovim/neovim/issues/12242
-      -- vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
-      --   callback = function ()
-      --     session_manager.save_current_session()
-      --   end
-      -- })
-    end,
+    config = function(_, opts) require("session_manager").setup(opts) end,
   },
 
   -- spectre.nvim [search and replace in project]
